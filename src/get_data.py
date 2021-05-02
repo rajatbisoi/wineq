352e099666b281ddbca9f63047a00e1102f9e58c
rajatbisoi1 <rajatbisoi1@gmail.com>
2021-05-03 00:12:25 +0530

    stage one compelete

--- src/get_data.py
+++ src/get_data.py
@@ -13,13 +13,12 @@ def read_params(config_path):
 
 def get_data(config_path):
     config = read_params(config_path)
-    print(config)
     data_path = config['data_source']['data_path']
     df = pd.read_csv(data_path,sep=",",encoding = 'utf-8')
-    print(df.head())
+    return df
 
 if __name__=="__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config",default="params.yaml")
     parsed_args = args.parse_args()
-    get_data(config_path=parsed_args.config)
+    data = get_data(config_path=parsed_args.config)
