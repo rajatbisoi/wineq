03c817372a7c4f12cbfe8f75b0c43ee3519dcdfb
rajatbisoi1 <rajatbisoi1@gmail.com>
2021-05-11 16:44:30 +0530

    try4

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -49,7 +49,7 @@ def validate_input(dict_request):
 
     def _validate_values(col, val):
         schema = get_schema()
-        if not (schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]):
+        if not (schema[col]["min"] <= float(*dict_request[col]) <= schema[col]["max"]):
             raise NotInRange
 
     for col,val in dict_request.items():
@@ -60,7 +60,7 @@ def validate_input(dict_request):
 def form_response(dict_request):
     if validate_input(dict_request):
         data = dict_request.values()
-        # data = np.array([ i[1][0] for i in dict(dict_request).items()]).reshape(1,-1)
+        data = np.array([ i[1][0] for i in dict(dict_request).items()]).reshape(1,-1)
         # print(data)
         data = [list(map(float,data))]
         response = predict(data)
